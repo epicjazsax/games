cc945b596f1b65b5e63e6577dbb8d9a87977240a
Justin Zakrajsek <justin.zakrajsek@gmail.com>
2026-02-26 10:17:17 -0800

    Break out validation logic into its own function

--- higher_lower.py
+++ higher_lower.py
@@ -1,40 +1,43 @@
 import random
 
-#add validate function for guess to replace first few if statements
-
 def prompt_for_guess():
     target = random.randrange(1, 101)
-    while True:
-        user_input = input('Guess the number between 1 and 100: ')
+    def validate_integer_in_range(user_input_string):
         try:
-            guess = int(user_input)
+            user_integer = int(user_input_string)
         except:
-            print('Please guess an integer')
-            continue
+            return False
 
-        if guess < 1:
-            print('Integer must be 1 or higher')
-        elif guess > 100:
-            print('Integer must be 100 or lower')
-        elif guess > target:
-            print('Lower!')
-        elif guess < target:
-            print('Higher!')
-        elif guess == target:
-            print(f'Congratulations! The correct answer was {guess}.')
+        if user_integer < 1 or user_integer > 100:
+            return False
 
-            def play_again_prompt():
-                return input('Play again? Y/N: ')
-            play_again_response = play_again_prompt().lower()
+        return True
 
-            if play_again_response == 'y':
-                prompt_for_guess()
-            elif play_again_response == 'n':
+    while True:
+        user_input = input('Guess the number from 1 to 100: ')
+        if validate_integer_in_range(user_input) == False:
+            print('Please guess an integer from 1 to 100')
+        else:
+            guess = int(user_input)
+            if guess > target:
+                print('Lower!')
+            elif guess < target:
+                print('Higher!')
+            elif guess == target:
+                print(f'Congratulations! The correct answer was {guess}.')
+
+                def play_again_prompt():
+                    return input('Play again? Y/N: ')
+                play_again_response = play_again_prompt().lower()
+
+                if play_again_response == 'y':
+                    prompt_for_guess()
+                elif play_again_response == 'n':
+                    break
+                else:
+                    print('Please answer Y or N')
+                    play_again_prompt()
                 break
-            else:
-                print('Please answer Y or N')
-                play_again_prompt()
-            break
 
 if __name__ == '__main__':
     prompt_for_guess()
