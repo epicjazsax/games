3033a2e70ea0709470b5a82e5065dd5be0f85ece
Justin Zakrajsek <justin.zakrajsek@gmail.com>
2026-03-02 11:07:24 -0800

    Refactor RPS Play Again Prompt

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -12,25 +12,39 @@ def get_selection():
 
 
 def resolve(player_guess, computer_guess):
-    play_options = ['rock', 'paper', 'scissors']
     message = ['you win!', 'computer wins :(', 'tie. no one wins']
-    if player_guess and computer_guess in play_options:
-        result = ((play_options.index(player_guess) - play_options.index(computer_guess)) + 2) % 3
+    if player_guess and computer_guess in PLAY_OPTIONS:
+        result = ((PLAY_OPTIONS.index(player_guess) - PLAY_OPTIONS.index(computer_guess)) + 2) % 3
         return message[result]
 
+def play_again_prompt():
+    while True:
+        def prompt_user():
+            return input('Play again? Y/N: ')
+        user_response = prompt_user().lower()
+
+        if user_response == 'y':
+            return True
+        elif user_response == 'n':
+            return False
+        else:
+            print('Invalid answer')
+            continue
 
 def rock_paper_scissors():
     while True:
         computer_guess = PLAY_OPTIONS[randint(0, 2)]
         player_guess = get_selection()
-        print(f'computer chose {computer_guess}')
-        resolution = resolve(player_guess, computer_guess)
-        print(resolution)
-        play_again = input('do you want to continue? yes or no: ')
-        if play_again == 'no':
-            print('have a nice day <3')
-            break
 
+        resolution = resolve(player_guess, computer_guess)
+        print(f'computer chose {computer_guess}')
+        print(resolution)
+
+        play_again_response = play_again_prompt()
+
+        if play_again_response == True:
+            rock_paper_scissors()
+        break
 
 if __name__ == '__main__':
     rock_paper_scissors()
